cebfb0c473a00bb483fda81c5dcd5a146a307714
szymanskiburgos <77420949+szymanskiburgos@users.noreply.github.com>
2024-02-28 10:24:28 -0700

    Add files via upload

---
+++ cQuant Coding Exercise.R
@@ -0,0 +1,12 @@
+# cQuant Coding Exercise
+# Adam Szymanski-Burgos
+# Created: 02/29/2024
+
+# Packages =====================================================================
+library(tidyverse)
+library(factoextra)
+library(Compind)
+library(scales) 
+library(gridExtra)
+library(foreign)
+
